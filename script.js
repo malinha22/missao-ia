64b3c70be1bfddf041be632f698dab45daf93554
malinha22 <bruga.amalia@escola.pr.gov.br>
2026-09-17 10:36:52 -0300

    .

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++
     mostraPergunta();
